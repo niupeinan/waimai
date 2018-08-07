0cbb6b250c458e61aee5d0e6198b7049361a01d4
Your Name <867761374@qq.com>
2018-08-07 22:40:30 +0800

    1

---
+++ js/rem1.js
@@ -0,0 +1,26 @@
+/*
+* @Author: 牛培楠
+* @Date:   2018-07-28 17:31:46
+* @Last Modified by:   牛培楠
+* @Last Modified time: 2018-07-28 17:45:17
+*/
+(function (doc, win) {
+        var docEl = doc.documentElement,
+            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function () {
+                var clientWidth = docEl.clientWidth;
+                if (!clientWidth) return;
+                if(clientWidth>=750){
+                  // 这里的640 取决于设计稿的宽度
+                    docEl.style.fontSize = '100px';
+                }else{
+                    docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+                }     
+            };
+
+        if (!doc.addEventListener) return;
+        win.addEventListener(resizeEvt, recalc, false);
+        doc.addEventListener('DOMContentLoaded', recalc, false);
+    })(document, window);
+
+
